47922fc26cbfa864a9afbc0829d1bd42758700d5
Jevin <therealjevin@gmail.com>
2023-01-10 08:57:00 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const feetPerBlock = 264;
+function distanceFromHqInBlocks(street) {
+    return Math.abs(42 - street);
+}
+
+function distanceFromHqInFeet(street) {
+    return feetPerBlock * distanceFromHqInBlocks(street);
+}
+
+function distanceTravelledInFeet(src, dest) {
+    return feetPerBlock * Math.abs(dest - src);
+}
+
+function calculatesFarePrice(start, destination) {
+    const feetTravelled = distanceTravelledInFeet(start, destination);
+    switch(true)
+    {
+        case feetTravelled <= 400:
+            return 0;
+        case feetTravelled <= 2000:
+            return 0.02 * (feetTravelled - 400);
+        case feetTravelled <= 2500:
+            return 25;
+        default:
+            return 'cannot travel that far';
+    }
+}
